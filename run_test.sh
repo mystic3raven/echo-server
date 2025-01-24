f6183cccb70512f36db559fd30413c893e194eea
mystic3raven <mystic3eyedraven@gmail.com>
2025-01-23 17:18:02 -0800

    modified the test scripts

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,5 @@
-curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello"}' http://localhost:5000/echo
+#!/usr/bin/env bash
+message="$(./random_str.sh)"
+curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"$message\"}" http://localhost:5001/echo
+
 # curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello, ECS!"}' http://127.0.0.1:5000/echo
